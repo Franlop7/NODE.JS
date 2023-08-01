8fa97f29a3fd67d9f5bbdd36f547b9366209e636
Fran Lopez <franciscolopto@gmail.com>
2023-08-01 20:55:46 +0200

    add fs readfilesync and callback

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,11 +1,18 @@
 const fs = require('node:fs')
 
 console.log('Leyendo el primer archivo ...')
-const text = fs.readFileSync('./archivo.txt', 'utf-8')
+// const text = fs.readFileSync('./archivo.txt', 'utf-8')
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => { // <-- efecutas este callback
+    console.log('Primer texto:', text)
+})
 
-console.log(text)
+// console.log(text)
+console.log('Hacer cosas mientras lee el archivo ...')
 
 console.log('Leyendo el segundo archivo ...')
-const secondText = fs.readFileSync('./archivo2.txt', 'utf-8')
+// const secondText = fs.readFileSync('./archivo2.txt', 'utf-8')
+fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
+    console.log('Segundo texto:', text)
+})
 
-console.log(secondText)
+// console.log(secondText)
