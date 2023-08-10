13988bc9cf9de40853fb0333f0fe71988966ef2b
Fran Lopez <franciscolopto@gmail.com>
2023-08-10 21:01:49 +0200

    update app ls

--- 6.ls.js
+++ 6.ls.js
@@ -1,13 +1,25 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 
-fs.readdir('.', (err, files) => {
+// fs.readdir('.', (err, files) => {
+//     if (err) {
+//         console.error('Error al leer el directorio: ', err)
+//         return;
+//     }
+
+//     files.forEach(files => {
+//         console.log(files)
+//     })
+// })
+
+fs.readdir('.')
+    .then(files => {
+        files.forEach(file => {
+        console.log(file)
+    })
+})
+    .catch(err => {
     if (err) {
         console.error('Error al leer el directorio: ', err)
         return;
-    }
-
-    files.forEach(files => {
-        console.log(files)
+        }
     })
-})
-
