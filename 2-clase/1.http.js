7ec47853a192b55e186708914b99e64cbe464b83
Fran Lopez <franciscolopto@gmail.com>
2023-08-21 20:01:38 +0200

    update http and set header charset utf-8

--- 2-clase/1.http.js
+++ 2-clase/1.http.js
@@ -2,10 +2,22 @@ const http = require('node:http')
 
 const desiredPort = process.env.Port ?? 3000
 
-const server = http.createServer((req, res) => {
-  console.log('request received: ', req.url)
-  res.end('Hola Mundo')
-})
+const processRequest = (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200 // ok
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end('<h1>Bienvenido a mi página de inicio</h1>')
+  }
+}
+
+const server = http.createServer(processRequest)
+
+// los mismo que el codigo de arriba pero de forma conjunta, arriba esta separado.
+
+// const server = http.createServer((req, res) => {
+//   console.log('request received: ', req.url)
+//   res.end('Hola Mundo')
+// })
 
 server.listen(desiredPort, () => {
   console.log(`server listening on port http://localhost:${desiredPort}`)
