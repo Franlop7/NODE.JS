8cdc0bbc56122f3996f38ffd91cc6a9210d2b038
Fran Lopez <franciscolopto@gmail.com>
2023-08-23 12:17:46 +0200

    add server express

---
+++ 2-clase/3.express.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const ditto = require('./pokemon/ditto.json')
+// const path = require('path')
+
+const PORT = process.env.PORT ?? 3000
+
+const app = express()
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//   // trackear la request a la base de datos
+//   // revisar si el usuario tiene cookies
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header content-type: aplication/json
+//   let body = ''
+
+//   // escuhar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la información en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
+})
+
+app.post('/pokemon', (req, res) => {
+  // req.body deberíamos guardar en una base de datos
+  res.status(201).json(req.body)
+})
+
+// la última a la que va a llegar
+app.use((req, res) => {
+  res.status(404).send('<h1>Error 404</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
